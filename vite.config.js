a53f236b99bfd8550a32789c4b7a7081a9d8a906
Gordon Zhao <gordonzhao7@gmail.com>
2023-02-07 19:11:57 -0800

    inital commit

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+	resolve: {
+		alias: [
+			{
+				find: 'common',
+				replacement: resolve(__dirname, 'src/common'),
+			},
+		],
+	},
+
+	plugins: [react()],
+});
