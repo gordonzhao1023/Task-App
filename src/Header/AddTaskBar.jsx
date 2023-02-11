a52599335433f77fba2ee0fadab67c77cbfeeaa8
Gordon Zhao <gordonzhao7@gmail.com>
2023-02-11 08:52:48 -0800

    fixed AddTaskBar

--- src/Header/AddTaskBar.jsx
+++ src/Header/AddTaskBar.jsx
@@ -1,3 +1,19 @@
-const AddTaskBar = () => {};
+import React, { useReducer } from 'react';
+
+const AddTaskBar = () => {
+	const taskRef = useRef('');
+	return (
+		<form>
+			<input
+				type=''
+				id='country-search'
+				placeholder='Add your task here...'
+				ref={taskRef}
+				// className=
+			/>
+			<button>ADD</button>
+		</form>
+	);
+};
 
 export default AddTaskBar;
